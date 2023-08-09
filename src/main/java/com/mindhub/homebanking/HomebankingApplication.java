1ba60ad318eb255715698b014b1515b263b12831
Ivan <ivangiannini1993@gmail.com>
2023-08-08 22:07:53 -0300

    sprint2: add model, repository Account. controllers

--- src/main/java/com/mindhub/homebanking/HomebankingApplication.java
+++ src/main/java/com/mindhub/homebanking/HomebankingApplication.java
@@ -1,11 +1,16 @@
 package com.mindhub.homebanking;
 
+import com.mindhub.homebanking.models.Account;
 import com.mindhub.homebanking.models.Client;
+import com.mindhub.homebanking.repositories.AccountRepository;
 import com.mindhub.homebanking.repositories.ClientRepository;
+import net.bytebuddy.asm.Advice;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDate;
 
 @SpringBootApplication
 public class HomebankingApplication {
@@ -15,14 +20,20 @@ public class HomebankingApplication {
 	}
 
 	@Bean
-	public CommandLineRunner init(ClientRepository clientRepository){
+	public CommandLineRunner init(ClientRepository clientRepository, AccountRepository accountRepository){
 		return args -> {
 			Client client = new Client();
-			client.setFirstName("Homer");
-			client.setLastName("Simpson");
-			client.setEmail("homer.simpson@gmail.com");
-
+			client.setFirstName("Melba");
+			client.setLastName("Morel");
+			client.setEmail("melba.morel@gmail.com");
 			clientRepository.save(client);
+
+			LocalDate date1 = LocalDate.now();
+			Account account1 = new Account("VIN001", date1, 5000.0, client);
+			LocalDate date2 = date1.plusDays(1);
+			Account account2 = new Account("VIN002", date2, 7500.0, client);
+			accountRepository.save(account1);
+			accountRepository.save(account2);
 		};
 	}
 }
